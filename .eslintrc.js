0a6248f3065c916a79cda58575ae507732944d3f
yuadh <1912546219@qq.com>
2023-02-25 17:57:38 +0800

    xasa

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// .eslintrc.js
+console.log(" INFO  读取了: .eslintrc.js");
+module.exports = {
+  extends: [
+    "airbnb-base", // airbnb要求字符串是单引号
+    "plugin:prettier/recommended", // 新增，必须放在最后面
+  ],
+  plugins: ["prettier"],
+  rules: {
+    semi: [2, "never"],
+    "prettier/prettier": "error",
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+// .prettierrc.js
+console.log(" INFO  读取了: .prettierrc.js");
+module.exports = {
+  singleQuote: false, // 默认为false。即要求：const a = "1";，可改为true，即要求const a = '1';
+};
